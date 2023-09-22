2b666ba91cd1b3e69540cfea3f51d84c58fa320f
Стас <@DESKTOP-0OF5UJB>
2023-09-23 02:14:04 +0300

    Добавьте файлы проекта.

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
