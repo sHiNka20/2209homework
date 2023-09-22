a1af445a155f5cb0f13ed7478ef20fd1f1dc7156
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-09-23 02:25:12 +0300

    Add files via upload

---
+++ Task66.cs
@@ -0,0 +1,30 @@
+﻿
+void PrintSumm(int m, int n, int summ)
+{
+    summ = summ + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов между M и N = {summ} ");
+        return;
+    }
+    PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSumm(m, n, temp=0);
